639cc5c9bac71708d7db6828b23df8978c2b2afe
bhagavandas <98629370+bhagavandas@users.noreply.github.com>
2022-08-31 06:55:19 +0530

    Added: Methods of checking file exists or not and deletes the file and create the files, directory

---
+++ src/main/java/com/bl/EmpPayrollServices/FileUtils.java
@@ -0,0 +1,16 @@
+package com.bl.EmpPayrollServices;
+
+import java.io.File;
+
+public class FileUtils {
+
+    public static boolean deleteFiles(File contentsToDelete) {
+        File[] allContents = contentsToDelete.listFiles();
+        if (allContents != null) {
+            for (File file : allContents) {
+                deleteFiles(file);
+            }
+        }
+        return contentsToDelete.delete();
+    }
+}
---
+++ src/test/java/com/bl/EmpPayrollServices/NIOFileAPITest.java
@@ -0,0 +1,48 @@
+package com.bl.EmpPayrollServices;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.IntStream;
+
+class NIOFileAPITest extends FileUtils {
+    private static final String HOME = System.getProperty("user.home");
+    private static final String PLAY_WITH_NIO = "TempPlayGround";
+
+    @Test
+    public void givenPathWhenCheckedThenConfirm() throws IOException {
+
+        Path currentDirPath = Paths.get(HOME);
+        Assertions.assertTrue(Files.exists(currentDirPath));
+
+        Path playPath = Paths.get(HOME + "/" + PLAY_WITH_NIO);
+        //deletes the files
+        if(Files.exists(playPath)) FileUtils.deleteFiles(playPath.toFile());
+        Assertions.assertTrue(Files.notExists(playPath));
+        //creates the directory
+        Files.createDirectory(playPath);
+        Assertions.assertTrue(Files.exists(playPath));
+        IntStream.range(1,10).forEach(cntr ->{
+            Path tempFile = Paths.get(playPath + "/temp" +cntr);
+            Assertions.assertTrue(Files.notExists(tempFile));
+            try{
+                Files.createFile(tempFile);
+            }catch (IOException e){
+                Assertions.assertTrue(Files.exists(tempFile));
+            }
+        });
+        //List the files
+        Files.list(playPath).filter(Files::isRegularFile).forEach(System.out::println);
+        Files.newDirectoryStream(playPath).forEach(System.out::println);
+        Files.newDirectoryStream(playPath, path ->
+                path.toFile().isFile() && path.toString().
+                        startsWith("temp")).forEach(System.out::println);
+    }
+
+
+
+}
